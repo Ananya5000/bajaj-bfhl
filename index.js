6e7991e0735626f49aecebc728aab21bc7ad45fb
Ananya5000 <ananya.2022@vitstudent.ac.in>
2025-08-29 11:37:22 +0530

    Added UI and updated API logic

--- index.js
+++ index.js
@@ -1,57 +1,62 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+const path = require("path");
 const app = express();
-app.use(bodyParser.json());
 
-app.post("/bfhl", (req, res) => {
-  try {
-    const data = req.body.data || [];
+app.use(express.json());
 
-    let odd_numbers = [];
-    let even_numbers = [];
-    let alphabets = [];
-    let special_characters = [];
-    let sum = 0;
-
-    data.forEach((str) => {
-      if (/^-?\d+$/.test(str)) {
-        const num = parseInt(str);
-        sum += num;
-        if (num % 2 === 0) {
-          even_numbers.push(str);
-        } else {
-          odd_numbers.push(str);
-        }
-      } else if (/^[a-zA-Z]+$/.test(str)) {
-        alphabets.push(str.toUpperCase());
-      } else {
-        special_characters.push(str);
-      }
-    });
-    let allChars = alphabets.join("");
-    let reversed = allChars.split("").reverse().join("");
-    let concat_string = "";
-    for (let i = 0; i < reversed.length; i++) {
-      concat_string += i % 2 === 0 ? reversed[i].toUpperCase() : reversed[i].toLowerCase();
-    }
-
-    res.json({
-      is_success: true,
-      user_id: "ananya_10072004",
-      email: "ananya,2022@vitstudent.ac.in",
-      roll_number: "22BEI0073",
-      odd_numbers,
-      even_numbers,
-      alphabets,
-      special_characters,
-      sum: sum.toString(),
-      concat_string,
-    });
-  } catch (error) {
-    res.json({ is_success: false, error: error.message });
-  }
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const USER_ID = "ananya_10072004";
+const EMAIL = "ananya.2022@vitstudent.ac.in";
+const ROLL_NUMBER = "22BEI0073";
+
+app.post("/bfhl", (req, res) => {
+  const data = req.body.data;
+
+  let odd_numbers = [];
+  let even_numbers = [];
+  let alphabets = [];
+  let special_characters = [];
+  let sum = 0;
+  let allChars = "";
+
+  data.forEach((str) => {
+    if (/^-?\d+$/.test(str)) {
+      let num = parseInt(str);
+      if (num % 2 === 0) even_numbers.push(str);
+      else odd_numbers.push(str);
+      sum += num;
+    } else if (/^[a-zA-Z]+$/.test(str)) {
+      alphabets.push(str.toUpperCase());
+      allChars += str;
+    } else {
+      special_characters.push(str);
+    }
+  });
+
+  let reversed = allChars.split("").reverse().join("");
+  let concat_string = "";
+  for (let i = 0; i < reversed.length; i++) {
+    concat_string += i % 2 === 0 ? reversed[i].toUpperCase() : reversed[i].toLowerCase();
+  }
+
+  res.json({
+    is_success: true,
+    user_id: USER_ID,
+    email: EMAIL,
+    roll_number: ROLL_NUMBER,
+    odd_numbers,
+    even_numbers,
+    alphabets,
+    special_characters,
+    sum: sum.toString(),
+    concat_string,
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
