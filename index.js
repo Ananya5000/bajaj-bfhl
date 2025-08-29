87d755992a0dd888e02d7830baf495a275be4fa1
Ananya5000 <ananya.2022@vitstudent.ac.in>
2025-08-29 11:00:58 +0530

    Initial commit - Bajaj BFHL API

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.json());
+
+app.post("/bfhl", (req, res) => {
+  try {
+    const data = req.body.data || [];
+
+    let odd_numbers = [];
+    let even_numbers = [];
+    let alphabets = [];
+    let special_characters = [];
+    let sum = 0;
+
+    data.forEach((str) => {
+      if (/^-?\d+$/.test(str)) {
+        // number
+        const num = parseInt(str);
+        sum += num;
+        if (num % 2 === 0) {
+          even_numbers.push(str);
+        } else {
+          odd_numbers.push(str);
+        }
+      } else if (/^[a-zA-Z]+$/.test(str)) {
+        // full alphabetic string
+        alphabets.push(str.toUpperCase());
+      } else {
+        // special char
+        special_characters.push(str);
+      }
+    });
+
+    // concat_string: all alphabets joined, reversed, alternating caps
+    let allChars = alphabets.join("");
+    let reversed = allChars.split("").reverse().join("");
+    let concat_string = "";
+    for (let i = 0; i < reversed.length; i++) {
+      concat_string += i % 2 === 0 ? reversed[i].toUpperCase() : reversed[i].toLowerCase();
+    }
+
+    res.json({
+      is_success: true,
+      user_id: "ananya_10072004",
+      email: "ananya,2022@vitstudent.ac.in",
+      roll_number: "22BEI0073",
+      odd_numbers,
+      even_numbers,
+      alphabets,
+      special_characters,
+      sum: sum.toString(),
+      concat_string,
+    });
+  } catch (error) {
+    res.json({ is_success: false, error: error.message });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
